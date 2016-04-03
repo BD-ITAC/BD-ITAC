de8ab186322a5f1351198d4d2579fd7a83524ce1
Wilson Critoni Neto <wcristoni@gmail.com>
2016-04-03 19:35:04 -0300

    Versão 1 do BD-ITAC App
    Esta é a versão 01 do BD-ITAC App para Android.
    Já possui o esquema de navegação entre a tela principal e as demais
    telas (cadastro e consulta de eventos e consulta de indicadores.

---
+++ BD-ITAC/app/src/main/java/br/ita/bd_itac/ConsultaEventoActivity.java
@@ -0,0 +1,13 @@
+package br.ita.bd_itac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultaEventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_evento);
+    }
+}
---
+++ BD-ITAC/app/src/main/java/br/ita/bd_itac/ConsultarIndicadorActivity.java
@@ -0,0 +1,13 @@
+package br.ita.bd_itac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultarIndicadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_indicador);
+    }
+}
