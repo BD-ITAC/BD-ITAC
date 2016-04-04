c45f35fa0b43c38e289db75f62367418d963e5d6
jm4rc05 <jose.marcos.gomes@gmail.com>
2016-04-04 18:48:13 -0300

    Separar a geração das notificações do polling ao serviço de alertas

--- Alertas/app/src/main/java/br/ita/bditac/mobile/alertas/AlertaPollingService.java
+++ Alertas/app/src/main/java/br/ita/bditac/mobile/alertas/AlertaPollingService.java
@@ -2,9 +2,6 @@ package br.ita.bditac.mobile.alertas;
 
 import android.Manifest;
 import android.app.IntentService;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,29 +15,11 @@ import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
-import java.util.List;
-
-import br.ita.bditac.ws.client.AlertaClient;
-import br.ita.bditac.ws.model.Alerta;
-
 public class AlertaPollingService extends IntentService {
-
-    // TODO Definir a localização padrão do serviço BD-ITAC em produção
-    private static final String DEFAULT_URL = "http://localhost:8080/alerta";
-
-    private static final String DEBUG_URL = "http://localhost:8080/alerta";
-
-    private static final String DEFAULT_RADIUS_KMS = "10";
-
-    private static final double DEBUG_RADIUS_KMS = 1;
 
     private static final int DEFAULT_LOCATION_POLLING_INTERVAL = 120000;
 
     private static final int DEBUG_LOCATION_POLLING_INTERVAL = 15000;
-
-    private String alertasUrl;
-
-    private Double radiusKms;
 
     private int locationPollingInterval;
 
@@ -57,16 +36,6 @@ public class AlertaPollingService extends IntentService {
         if(!Debug.isDebuggerConnected()) {
             preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         }
-
-        alertasUrl =
-                Debug.isDebuggerConnected() ?
-                        DEBUG_URL :
-                        preferences.getString("alerts.service.url", DEFAULT_URL);
-
-        radiusKms =
-                Debug.isDebuggerConnected() ?
-                        DEBUG_RADIUS_KMS :
-                        new Double(preferences.getString("alerts.service.radiusKms", DEFAULT_RADIUS_KMS)).doubleValue();
 
         locationPollingInterval =
                 Debug.isDebuggerConnected() ?
@@ -139,38 +108,6 @@ public class AlertaPollingService extends IntentService {
                 currentLocation = location;
             }
 
-            AlertaClient alertaClient = new AlertaClient(alertasUrl);
-
-            List<Alerta> alertas = alertaClient.getAlertaByRegiao(location.getLatitude(), location.getLongitude(), radiusKms);
-
-            if(alertas == null) {
-                Log.i(this.getClass().getSimpleName(), "Alerts not found for now.");
-            }
-            else {
-                Context context = getApplicationContext();
-
-                for (Alerta alerta : alertas) {
-                    Intent getAlertaIntent = new Intent(context, NotificationReceiverActivity.class);
-                    PendingIntent pGetAlertaIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), getAlertaIntent, 0);
-
-                    Notification notification = new Notification.Builder(context)
-                            .setContentTitle(context.getText(R.string.msg_alert_received))
-                            .setContentText(alerta.getDescricaoResumida())
-                            .setStyle(new Notification.BigTextStyle().bigText(alerta.getDescricaoCompleta()))
-                            .setSmallIcon(R.drawable.ic_action_alert)
-                            .setContentIntent(pGetAlertaIntent)
-                            .addAction(R.drawable.ic_stat_alert, context.getText(R.string.open), pGetAlertaIntent)
-                            .build();
-                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-
-                    notification.flags |= Notification.FLAG_AUTO_CANCEL;
-
-                    notificationManager.notify(NotificationIdGenerator.getNewID(), notification);
-
-                    Log.w(this.getClass().getSimpleName(), "Notification posted.");
-                }
-            }
-
         }
 
         @Override
@@ -205,32 +142,22 @@ public class AlertaPollingService extends IntentService {
             Context context = getApplicationContext();
 
             LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-//            Criteria criteria = new Criteria();
-//            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-//            criteria.setAltitudeRequired(false);
-//            criteria.setBearingRequired(false);
-//            criteria.setCostAllowed(true);
-//            String bestProvider = locationManager.getBestProvider(criteria, true);
-//            if(bestProvider == null) {
-//                Log.e(this.getClass().getSimpleName(), "Location service providers not found.");
-//            }
-//            else {
             if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 Log.i(this.getClass().getSimpleName(), "Location service registered.");
 
                 LocationListener locationListener = new AlertaLocationListener();
-//                    locationManager.requestLocationUpdates(bestProvider, 0, 0, locationListener);
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                AlertaGetter alertaGetter = new AlertaGetter(getApplicationContext());
+
+                alertaGetter.fetchAlertas(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
             }
 
             Log.i(this.getClass().getSimpleName(), "Background task finished.");
-//            }
         }
         catch(Exception ex) {
             Log.e(this.getClass().getSimpleName(), ex.getMessage(), ex);
         }
-
-        Log.w(this.getClass().getSimpleName(), "Location services service unavailable.");
 
     }
 
