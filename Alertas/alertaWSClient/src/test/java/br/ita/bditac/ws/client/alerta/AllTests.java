e793dac0a4df2506009c9658ae0604e5b6438283
jm4rc05 <jose.marcos.gomes@gmail.com>
2016-04-07 13:32:17 -0300

    Implementando testes unitários no ambiente do Android Studio

--- Alertas/alertaWSClient/src/test/java/br/ita/bditac/ws/client/alerta/AllTests.java
+++ Alertas/alertaWSClient/src/test/java/br/ita/bditac/ws/client/alerta/AllTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
 @SuiteClasses({ EventoTests.class, AlertaTests.class })
 public class AllTests {
 
