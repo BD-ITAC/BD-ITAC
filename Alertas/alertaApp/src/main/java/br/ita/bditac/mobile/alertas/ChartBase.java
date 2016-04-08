1facddf20b65870f5e690c08b383bb5cf9a7e5e5
jm4rc05 <jose.marcos.gomes@gmail.com>
2016-04-08 17:13:04 -0300

    Merge dos dois sub-projetos e inclusão da lib MPChartLib

---
+++ Alertas/alertaApp/src/main/java/br/ita/bditac/mobile/alertas/ChartBase.java
@@ -0,0 +1,29 @@
+package br.ita.bditac.mobile.alertas;
+
+
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * Created by marcos on 4/7/16.
+ */
+public class ChartBase extends FragmentActivity {
+
+    protected String[] mMonths = new String[] {
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
+    };
+
+    protected String[] mParties = new String[] {
+            "Abertos", "Em andamento", "Encerrados", "Party D", "Party E", "Party F", "Party G", "Party H",
+            "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
+            "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+            "Party Y", "Party Z"
+    };
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.move_left_in_activity, R.anim.move_right_out_activity);
+    }
+
+}
