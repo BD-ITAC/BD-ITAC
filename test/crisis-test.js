1915b4b0caa14f583dc39c07d52ff6fd31bd1158
Danilo Ramalho <danilovex@gmail.com>
2016-04-09 17:51:01 -0300

    Testes validando erros da api de post

--- test/crisis-test.js
+++ test/crisis-test.js
@@ -10,16 +10,80 @@ describe("Test of Crisis",function(){
     // calling home page api
     server
     .post("/rest/crisis")
-    .send({'name': 'Java', 'email': 'teste@teste.com'})
+    .send({'name': 'João', 'email': 'teste@teste.com'})
     .expect("Content-type",/json/)
+    .set('Accept', 'application/json')
     .expect(404) // THis is HTTP response
     .end(function(err,res){
 
       res.should.be.json;
       // Error key should havebe false.
       res.body.should.have.property('success');
+      res.body.success.should.equal(false);
       res.body.should.have.property('message');
       res.body.message.should.equal('Required fields not informed.');
+
+      done();
+    });
+  });
+
+  it("Deve retornar erro ao passar email inválido",function(done){
+
+    var crisis_param = {
+      name: 'Teste',
+      email: 'testegmail.com',
+      phone: 111111,
+      place: 'Teste',
+      type: 4,
+      title: 'Teste'
+    };
+
+    // calling home page api
+    server
+    .post("/rest/crisis")
+    .send(crisis_param)
+    .expect("Content-type",/json/)
+    .set('Accept', 'application/json')
+    .expect(500) // THis is HTTP response
+    .end(function(err,res){
+
+      res.should.be.json;
+      // Error key should havebe false.
+      res.body.should.have.property('success');
+      res.body.success.should.equal(false);
+      res.body.should.have.property('message');
+      res.body.message.should.equal('Invalid value data fields.');
+
+      done();
+    });
+  });
+
+  it("Deve retornar erro ao passar tipo de crise fora do intervalo de 1 à 8",function(done){
+
+    var crisis_param = {
+      name: 'Teste',
+      email: 'teste@gmail.com',
+      phone: 111111,
+      place: 'Teste',
+      type: 9,
+      title: 'Teste'
+    };
+
+    // calling home page api
+    server
+    .post("/rest/crisis")
+    .send(crisis_param)
+    .expect("Content-type",/json/)
+    .set('Accept', 'application/json')
+    .expect(500) // THis is HTTP response
+    .end(function(err,res){
+
+      res.should.be.json;
+      // Error key should havebe false.
+      res.body.should.have.property('success');
+      res.body.success.should.equal(false);
+      res.body.should.have.property('message');
+      res.body.message.should.equal('Invalid value data fields.');
 
       done();
     });
