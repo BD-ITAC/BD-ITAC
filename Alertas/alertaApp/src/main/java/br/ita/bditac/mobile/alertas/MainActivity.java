d5ca1074e7fafc02499076e86c88cacecd5b214c
jm4rc05 <jose.marcos.gomes@gmail.com>
2016-04-08 23:53:26 -0300

    Mover requisição de permissão ao serviço de localização para a inicialização do aplicativo

--- Alertas/alertaApp/src/main/java/br/ita/bditac/mobile/alertas/MainActivity.java
+++ Alertas/alertaApp/src/main/java/br/ita/bditac/mobile/alertas/MainActivity.java
@@ -1,17 +1,36 @@
 package br.ita.bditac.mobile.alertas;
 
+
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ACCESS_FINE_LOCATION = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.i(this.getClass().getSimpleName(), "Permission to localization services requested.");
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_ACCESS_FINE_LOCATION);
+        }
+
     }
 
     // TODO: Fazer uma função que generaliza as funções start
@@ -30,4 +49,21 @@ public class MainActivity extends AppCompatActivity {
         Intent itentConsultarIndicador = new Intent(this, ConsultarIndicadorActivity.class);
         startActivity(itentConsultarIndicador);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CODE_ACCESS_FINE_LOCATION: {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(this.getClass().getSimpleName(), "Localization services permission granted.");
+                }
+                else {
+                    Log.e(this.getClass().getSimpleName(), "Localization services permission not granted..");
+                }
+            }
+        }
+
+    }
+
 }
