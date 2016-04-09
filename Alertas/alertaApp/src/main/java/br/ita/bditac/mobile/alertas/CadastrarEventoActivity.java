fd56852e51924d1704098e785ab8d9fcdc9c80fb
jm4rc05 <jose.marcos.gomes@gmail.com>
2016-04-08 21:30:40 -0300

    Integrar tela cadastro de eventos com cliente de serviço web

--- Alertas/alertaApp/src/main/java/br/ita/bditac/mobile/alertas/CadastrarEventoActivity.java
+++ Alertas/alertaApp/src/main/java/br/ita/bditac/mobile/alertas/CadastrarEventoActivity.java
@@ -1,6 +1,12 @@
 package br.ita.bditac.mobile.alertas;
 
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,11 +15,49 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 
+import br.ita.bditac.ws.client.EventoClient;
+import br.ita.bditac.ws.model.Evento;
+import br.ita.bditac.ws.model.Usuario;
+
 
 public class CadastrarEventoActivity extends AppCompatActivity {
 
+    // TODO Definir a localização padrão do serviço BD-ITAC em produção
+    private static final String DEFAULT_URL = "http://10.0.2.2:8080";
+
+    private static final String DEBUG_URL = "http://10.0.2.2:8080";
+
+    private String alertasUrl;
+
     private EditText inputDescricao;
+
     private Spinner inputCategoria;
+
+    private Context context;
+
+    private class SalvarEventoTask extends AsyncTask<Evento, Void, Void> {
+
+        private Exception exception;
+
+        protected Void doInBackground(Evento... eventos) {
+
+            try {
+                EventoClient eventoClient = new EventoClient(alertasUrl);
+
+                for(Evento evento: eventos) {
+                    eventoClient.addEvento(evento);
+                }
+            }
+            catch(Exception ex) {
+                this.exception = ex;
+            }
+            finally {
+                return null;
+            }
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +67,38 @@ public class CadastrarEventoActivity extends AppCompatActivity {
 
         inputDescricao = (EditText) findViewById(R.id.mensagem);
 
+        SharedPreferences preferences = null;
+
+        context = getApplicationContext();
+
+        if (!Debug.isDebuggerConnected()) {
+            preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        }
+
+        alertasUrl = Debug.isDebuggerConnected() ? DEBUG_URL : preferences.getString("alerts.service.url", DEFAULT_URL);
+
         CarregarCategoria();
 
     }
 
     public void Limpar_Click(View view) {
 
-        LimparTela();
+        inputDescricao.setText("");
+        inputCategoria.setSelection(0);
 
     }
 
     public void Salvar_Click(View view) {
 
-        SalvarDados();
+        Evento evento = new Evento();
+
+        evento.setDescricao(inputDescricao.getText().toString());
+        evento.setCategoria(inputCategoria.getId());
+        evento.setNome(Usuario.getNome());
+        evento.setEmail(Usuario.getEmail());
+        evento.setTelefone(Usuario.getTelefone());
+
+        new SalvarEventoTask().execute(evento);
 
     }
 
@@ -63,17 +126,6 @@ public class CadastrarEventoActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter <String>(this, android.R.layout.simple_dropdown_item_1line, categoriaArrayList);
         inputCategoria.setAdapter(adapter);
-
-    }
-
-    private void LimparTela(){
-
-        inputDescricao.setText("");
-        inputCategoria.setSelection(0);
-
-    }
-
-    private void SalvarDados(){
 
     }
 
