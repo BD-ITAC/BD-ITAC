4c2bcd4d7ce43125b94acc07e58052ae7caa842c
fabioajm <fabio_ajm@yahoo.com.br>
2016-04-10 17:01:25 -0300

    exemploo de teste com mock(stub)

--- test/crisis-business-test.js
+++ test/crisis-business-test.js
@@ -2,10 +2,12 @@
 var sinon = require('sinon');
 var app = require('../config/express')();
 var crisisBusiness = app.business.crisis;
+var should = require("should");
 
 
 describe('Crisis business', function() {
   describe('#save()', function() {
+  	 
     it('should save without error', function(done) {
      // var business = app.business.crisis;
       var crisis = {
@@ -19,16 +21,18 @@ describe('Crisis business', function() {
       
      
     var stub = sinon.stub(app.dao.crisis,'save');
-     stub.returns(function(crisis, callback){
-     	return callback(null, {status: 'ok'});
-     });
-     
+    stub.returns( function(crisis, callback){    
+     return callback(null, {status: 'ok'});
+    });
+    
 
-     crisisBusiness.save(crisis, function(err) {
-        if (err) throw err;
-        done();
+     crisisBusiness.save(crisis, function(err, data) {
+      
       });
 
+      stub.called.should.be.equal(true);
+     
+     done();
     });
   });
 });
