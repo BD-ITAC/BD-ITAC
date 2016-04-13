b9c258f1209801652e3cc3924786e299943f6c6b
Luiz Henrique Coura <luizcoura@gmail.com>
2016-04-13 00:42:41 -0300

    verificando propriedade fs.defaultFS

--- tests/test_conf_hadoop.py
+++ tests/test_conf_hadoop.py
@@ -1,11 +1,13 @@
+import os
+
 def test_core_site(File):
-    core = File("../etc/hadoop/core-site.xml")
-    assert core.contains("hdfs://localhost:9000")
+    core = File(os.getenv("HADOOP_HOME") + "/etc/hadoop/core-site.xml")
+    assert core.contains("fs.defaultFS")
     assert core.is_file
     assert core.mode == 0o644
 
 def test_hdfs_site(File):
-    hdfs = File("../etc/hadoop/hdfs-site.xml")
+    hdfs = File(os.getenv("HADOOP_HOME") + "/etc/hadoop/hdfs-site.xml")
     assert hdfs.contains("dfs.replication")
     assert hdfs.is_file
     assert hdfs.mode == 0o644
