c30b2a31fa396f0040ba7c6a693b455fdf669acc
Hadoop <hadoop@master.a.com>
2016-04-14 17:06:23 -0300

    renomeando arquivos para test_XX

--- tests/test_prerequisites.py
+++
@@ -1,8 +0,0 @@
-def test_ssh_is_installed(Package):
-    ssh = Package("openssh-server")
-    assert ssh.is_installed
-
-def test_ssh_running_and_enabled(Service):
-    ssh = Service("ssh")
-    assert ssh.is_running
-    assert ssh.is_enabled
