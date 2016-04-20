82497b41d1d70c4ddf0e077bd1d9ed8a149c10fc
Jose Marcos Gomes <marcos@Hyrielle.local>
2016-04-19 21:07:16 -0300

    Remoção de métodos em desuso

--- Alertas/alertaWSClient/src/main/java/br/ita/bditac/ws/client/CriseClient.java
+++ Alertas/alertaWSClient/src/main/java/br/ita/bditac/ws/client/CriseClient.java
@@ -1,15 +1,10 @@
 package br.ita.bditac.ws.client;
 
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import br.ita.bditac.ws.model.Crise;
-import br.ita.bditac.ws.model.CriseResource;
+import br.ita.bditac.ws.model.MessageResource;
 
 public class CriseClient extends AbstractBaseService {
 
@@ -21,40 +16,11 @@ public class CriseClient extends AbstractBaseService {
         super(hostURL);
     }
 
-    public Crise addCrise(Crise crise) {
+    public void addCrise(Crise crise) {
 
         HttpEntity envio = new HttpEntity(crise, getRequestHeader());
 
-        ResponseEntity<CriseResource> criseResponseEntity =  getRestTemplate().postForEntity(getHostURL() + SERVICE_URL, envio, CriseResource.class);
-
-        if(criseResponseEntity.getStatusCode() == HttpStatus.CREATED) {
-            return criseResponseEntity.getBody().getContent();
-        }
-        else {
-            return null;
-        }
-
-    }
-
-    public Crise getCriseById(int id) {
-
-        Map<String, Integer> params = new HashMap<String, Integer>();
-        params.put("id", id);
-
-        try {
-            HttpEntity envio = new HttpEntity(getResponseHeader());
-
-            ResponseEntity<CriseResource> criseResponseEntity = getRestTemplate().exchange(getHostURL() + SERVICE_URL + ID_PARM, HttpMethod.GET, envio, CriseResource.class, params);
-
-            if(criseResponseEntity.getStatusCode() == HttpStatus.OK) {
-                return criseResponseEntity.getBody().getContent();
-            }
-        }
-        catch(Exception ex) {
-
-        }
-
-        return null;
+        ResponseEntity<MessageResource> criseResponseEntity =  getRestTemplate().postForEntity(getHostURL() + SERVICE_URL, envio, MessageResource.class);
 
     }
 
