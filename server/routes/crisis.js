6dcc3fea11caa5a25f2863440ea389050c65618c
Andre Olimpio <professor.andre.olimpio@gmail.com>
2016-05-11 01:13:38 -0300

    Commit_TS1_SP1

--- server/routes/crisis.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app ){
-  var uri = '/rest/crisis';
-  var controller = app.controllers.crisis;
-  app.post(uri+'/', controller.saveCrisis);
-  app.get(uri+'/indicators', controller.listIndicators);
-};
