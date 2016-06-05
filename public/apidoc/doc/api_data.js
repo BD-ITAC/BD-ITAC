d9db0f244fbd2f8597611a4229559561eba6485d
Edizon Eduardo Basseto Junior <edizon.basseto@outlook.com>
2016-06-05 15:59:21 -0300

    Ajustes dos nomes na sala de situação e nos RESTS

---
+++ public/apidoc/doc/api_data.js
@@ -0,0 +1,30 @@
+define({ "api": [
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./main.js",
+    "group": "D__Projetos_ITA_BD_ITAC_public_apidoc_main_js",
+    "groupTitle": "D__Projetos_ITA_BD_ITAC_public_apidoc_main_js",
+    "name": ""
+  }
+] });
---
+++ public/js/controllers/avisosCtrl.js
@@ -0,0 +1,54 @@
+/*
+ * @author: edizon
+ */
+
+'use strict';
+app.controller('AvisosController', ['$scope', '$rootScope', '$location', '$http',
+  '$localStorage',
+  'toastr','UtilService','Lightbox','ModalService',
+  function($scope,$rootScope,$location,$http,$localStorage, toastr, UtilService, Lightbox, ModalService) {
+    var vm = this;
+    vm.form = {submitted : false};
+
+    $scope.avisosSelecionados = [];
+    UtilService.setCurrentMenu($scope, $location);
+
+    $http.get('/rest/avisos').success(function(data){
+                $scope.avisosListCollection = data;
+    });
+
+    $scope.openLightboxModal = function (index, fotos) {
+        $scope.images = [];
+          angular.forEach(fotos, function(value)
+          {
+              if(value != null && value)
+              {
+                  $scope.images.push({
+                   'url': 'data:image/gif;base64,' + value,
+                   'thumbUrl': 'data:image/gif;base64,' + value
+                  });
+              }
+          });
+          Lightbox.openModal($scope.images, index);
+    };
+
+    $scope.abrirCadastraCriseModal = function()
+    {
+
+      ModalService.showModal({
+            templateUrl: "/part-views/addCrisis.html",
+            controller: "addCrisisModalController",
+            inputs: { avisos: $scope.avisosSelecionados}
+
+      }).then(function(modal)
+        {
+          //it's a bootstrap element, use 'modal' to show it
+          modal.element.modal();
+          modal.close.then(function(result)
+          {
+            console.log(result);
+          });
+        });
+    }
+
+  }]);
