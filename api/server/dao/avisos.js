4e975b02cebbbfe7f0123bc6ae44bb0d571139cc
Edizon Eduardo Basseto Junior <edizon.basseto@outlook.com>
2016-06-06 15:44:27 -0300

    REST baseado no novo MER do BD

--- api/server/dao/avisos.js
+++ api/server/dao/avisos.js
@@ -28,43 +28,39 @@ avisosDAO = function(pool) {
     var dados = getDados(avisos);
     //Modificado por Edizon: utilizando transações por conta dos campos
     // auto incrementado.
+
     var query =
-    "insert into usuario set \n                  "+
-    "usu_nm = '" + dados.cri_nome + "',\n       "+
-    "usu_email = '" + dados.cri_email + "',\n    "+
-    "usu_fone = '" + dados.cri_telefone + "'; \n\n "+
+    "insert into usuario set \n             "+
+    "usu_nm = '" + dados.nome + "',\n       "+
+    "usu_email = '" + dados.email + "',\n   "+
+    "usu_app = 0, \n" +
+    "usu_pwd = '', \n" +
+    "usu_ultimo_login = NOW(), \n" +
+    "usu_fone = '" + dados.telefone + "'; \n\n "+
 
      "select @idUsuario:=usu_id from usuario where usu_id = LAST_INSERT_ID();\n\n" +
-     "insert into crise set \
-     cri_ds = '" + dados.cri_descricao + "',\n \
-     cri_ini = '" + dados.cri_dh_inicio + "',\n \
-     cri_atv = '" + dados.cri_ativo + "',\n \
-     cri_pic1 =?,\n \
-     cri_pic2 =?,\n \
-     cri_pic3 =?;\n\n"+
 
-     "select @idCrise:=cri_id from crise where cri_id = LAST_INSERT_ID();\n\n"+
+     "insert into aviso set \n \
+     usu_cod = @idUsuario, \n  \
+     sta_Cod = 1, \n \
+     geo_cod = '" + dados.geoCod + "', \n \
+     avs_ds = '" + dados.descricao + "',\n \
+     avs_latitude = '" + dados.latitude + "',\n \
+     avs_longitude = '" + dados.longitude + "',\n \
+     cat_cod = '" + dados.categoria + "';\n " +
 
-     "insert into geografica set \n\
-     geo_long = '"+dados.cri_longitude+"',\n\
-     geo_lat = '"+dados.cri_latitude+"';\n\n"+
+     "select @idCrise:=avs_id from aviso where avs_id = LAST_INSERT_ID();\n\n" +
 
-     "select @idGeo:=geo_id from geografica where geo_id = LAST_INSERT_ID();\n\n"+
-
-     "insert into ocorrencia set \n\
-     oco_cri_cod = @idCrise, \n\
-     oco_geo_cod = @idGeo, \n\
-     oco_usu_cod = @idUsuario, \n\
-     oco_eve_cod = '"+dados.cri_categoria + "';";
-
-     //console.log(query);
+     "insert into imagem set \n " +
+     "avs_cod = @idCrise, \n" +
+     "img_arq = ? ; ";
 
      db.executarTransacao(
         self.pool,
 
         function(conn)
         {
-            conn.query(query, [dados.cri_pic1, dados.cri_pic2, dados.cri_pic3]
+            conn.query(query, [dados.pic]
               ,function (err, result) {
                 if(err)
                 {
@@ -127,43 +123,39 @@ avisosDAO = function(pool) {
 
   function getDados(avisos){
     var dados={
-              cri_nome: avisos.nome,
-              cri_telefone: avisos.telefone,
-              cri_email: avisos.email,
-              cri_descricao: avisos.descricao,
-              cri_categoria: avisos.categoria,
-              cri_latitude: avisos.latitude,
-              cri_longitude: avisos.longitude,
-              cri_dh_inicio: new Date(),
-              cri_ativo: true,
-              cri_pic1: new Buffer(avisos.fotografia, 'base64'),
-              cri_pic2: '',
-              cri_pic3: ''
+              nome: avisos.nome,
+              telefone: avisos.telefone,
+              email: avisos.email,
+              descricao: avisos.descricao,
+              categoria: avisos.categoria,
+              latitude: avisos.latitude,
+              longitude: avisos.longitude,
+              geoCod: 1,
+              dh_inicio: new Date(),
+              ativo: true,
+              status: 1,
+              pic: new Buffer(avisos.fotografia, 'base64')
             }
     return dados;
   }
 
   dao.listAvisos = function(callback){
     var query =
-    "select cri.cri_id  as cri_id,             \
-             usu.usu_nm as cri_nome,       \
-            usu.usu_fone as cri_telefone,  \
-            usu.usu_email as cri_email,    \
-            cri.cri_ds as cri_descricao,   \
-            event.eve_ds as cri_categoria, \
-            geo.geo_lat as cri_latitude,   \
-            geo.geo_long as cri_longitude, \
-            cri.cri_ini as cri_dh_inicio,  \
-            cri.cri_fim as cri_dh_fim,      \
-            cri.cri_atv as cri_ativo,      \
-            cri.cri_pic1,                  \
-            cri.cri_pic2,                  \
-            cri.cri_pic3                   \
-       from  crise cri                     \
-        join ocorrencia ocor on ocor.oco_cri_cod = cri.cri_id  \
-        join usuario usu on usu.usu_id = oco_usu_cod           \
-        join geografica geo on geo.geo_id = oco_geo_cod        \
-        join evento event on event.eve_id = ocor.oco_eve_cod";
+    "select avs_id,                                    \
+        av.avs_ds as avs_ds,                           \
+        avs.sta_ds as sta_ds,                          \
+        av.avs_latitude as latitude,                   \
+        av.avs_longitude as longitude,                 \
+        cat.cat_ds as categoria,                       \
+        usu.usu_email,                                 \
+        usu.usu_fone,                                  \
+        usu.usu_nm,                                    \
+        img.img_arq                                    \
+   from aviso   av                                     \
+   join aviso_status avs on av.sta_cod = avs.sta_id    \
+   join categoria cat on av.cat_cod = cat.cat_id       \
+   join usuario usu on av.usu_cod = usu.usu_id         \
+   join imagem  img on img.avs_cod = av.avs_id";
 
 
     self.pool.query(
@@ -178,38 +170,32 @@ avisosDAO = function(pool) {
   };
 
   function getAvisos(rows){
-    var crises = [];
+    var avisos = [];
     for(c in rows){
 
       var pic1 = '';
-      var pic2 = '';
-      var pic3 = '';
 
-      if(rows[c].cri_pic1 != null) pic1 = new Buffer(rows[c].cri_pic1).toString('base64');
-      if(rows[c].cri_pic2 != null) pic2 = new Buffer(rows[c].cri_pic2).toString('base64');
-      if(rows[c].cri_pic3 != null) pic3 = new Buffer(rows[c].cri_pic3).toString('base64');
+      if(rows[c].img_arq != null) pic1 = new Buffer(rows[c].img_arq).toString('base64');
 
-      var crise={
-                id: rows[c].cri_id,
-                nome: rows[c].cri_nome,
-                telefone: rows[c].cri_telefone,
-                email: rows[c].cri_email,
-                descricao: rows[c].cri_descricao,
-                categoria: rows[c].cri_categoria,
-                latitude: rows[c].cri_latitude,
-                longitude: rows[c].cri_longitude,
-                dataInicial: rows[c].cri_dh_inicio,
-                dataFinal: rows[c].cri_dh_fim,
-                ativo: rows[c].cri_ativo,
-                pic1:   pic1,
-                pic2:   pic2,
-                pic3:   pic3
+      var aviso={
+                id: rows[c].avs_id,
+                nome: rows[c].usu_nm,
+                telefone: rows[c].usu_fone,
+                email: rows[c].usu_email,
+                descricao: rows[c].avs_ds,
+                categoria: rows[c].categoria,
+                latitude: rows[c].latitude,
+                longitude: rows[c].longitude,
+                //dataInicial: rows[c].cri_dh_inicio,
+                //dataFinal: rows[c].cri_dh_fim,
+                status: rows[c].sta_ds,
+                pic1:   pic1
               };
-              crises.push(crise);
+              avisos.push(aviso);
 
     }
 
-    return crises;
+    return avisos;
   }
 
 
