8441a3dc775d69f6fce855ed7db0672d1ba68251
Edizon Eduardo Basseto Junior <edizon.basseto@outlook.com>
2016-06-07 14:13:34 -0300

    REST Categorias buscando dados no BD

--- api/server/dao/category.js
+++ api/server/dao/category.js
@@ -1,23 +1,54 @@
+var db = require("../db/transacao");
 var categoryDao = null;
 
-
-module.exports = function() {
-  return categoryDao();
+module.exports = function(app) {
+  return categoryDao(app.db.conexao);
 }
 
-categoryDao = function() {
+categoryDao = function(pool) {
+    var self = this;
+    if (pool !== null) {
+        this.pool = pool;
+    }
 
   var dao = {};
 
-  var  categories = [];
+  dao.listAll = function(callback){
+    var query =
+    "SELECT           \
+        cat_id , \
+        cat_ds\
+      FROM categoria        \
+     ORDER BY cat_ds";
 
-  categories.push({id:1, descricao:'categoria 1'});
-  categories.push({id:2, descricao:'categoria 2'});
+    self.pool.query(
+       query,
+       function(err, rows){
+      if(err){
+        callback(err,{});
+      }else{
+        callback(null, getDados(rows));
+      }
+    });
+  };
 
+  function getDados(rows)
+  {
+    var categorias = [];
+    for(c in rows){
+      var categoria =
+      {
+        id: rows[c].cat_id,
+        description: rows[c].cat_ds
+      }
 
-  dao.listAll = function(callback) {
-    return callback(null,categories);
+      categorias.push(categoria);
+    }
+
+    console.log(categorias);
+    return categorias;
   }
+
   return dao;
 
 }
--- api/server/routes/category.js
+++ api/server/routes/category.js
@@ -2,7 +2,31 @@ module.exports = function(app){
 
   var uri = '/rest/categories';
   var controller = app.controllers.category;
-
+  /**
+   * @api {get} /rest/categories Listar
+   * @apiVersion 1.0.0
+   * @apiGroup Categorias
+   *
+   * @apiSuccess {Numero} categories.id Numero identificador.
+   * @apiSuccess {String} categories.description Descrição do aviso.
+   *
+   * @apiSuccessExample {json} Sucesso
+   * HTTP/1.1 200 OK
+   *    {
+   *      {
+   *          "id": "1",
+   *          "descricao" : "Alagamentos"
+   *      }
+   *      {
+   *          "id":"2",
+   *          "descricao" : "Emanação vulcânica"
+   *      }
+   *   }
+   *  @apiSampleRequest /rest/categories
+   */
   app.get(uri, controller.listAll);
+
+
+
 
 }
