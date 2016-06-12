8ea20f5b6bc644edc96e3c93a79dfead1e5d73b1
Edizon Eduardo Basseto Junior <edizon.basseto@outlook.com>
2016-06-11 23:04:02 -0300

    correcao no dao avisos

--- api/server/dao/avisos.js
+++ api/server/dao/avisos.js
@@ -19,6 +19,7 @@ avisosDAO = function(pool) {
   var dao = {};
 
   /**
+
   * Realiza registro de um novo aviso
   * @author Danilo Ramalho
   * @param objeto avisos
@@ -62,11 +63,14 @@ avisosDAO = function(pool) {
      avs_ptcoord=Point('"+dados.longitude+"','"+dados.latitude+"'),\n \
      cat_cod = '" + dados.categoria + "';\n\n " +
 
-     "select @idCrise:=avs_id from aviso where avs_id = LAST_INSERT_ID();\n\n" +
+     "select @idCrise:=avs_id from aviso where avs_id = LAST_INSERT_ID();\n\n"
 
-     "insert into imagem set \n " +
-     "avs_cod = @idCrise, \n" +
-     "img_arq = ? ; \n\n";
+     if(dados.pic != null && dados.pic.length > 0)
+     {
+       "insert into imagem set \n " +
+       "avs_cod = @idCrise, \n" +
+       "img_arq = ? ; \n\n";
+     }
 
      db.executarTransacao(
         self.pool,
