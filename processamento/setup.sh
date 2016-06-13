5032f12cea09c7e0baac87e40c32f933f1c66264
root <root@master.bditac.com>
2016-06-13 05:46:28 +0000

    modificando pasta confs para conf

--- processamento/setup.sh
+++ processamento/setup.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # Hadoop
-rsync -av /usr/local/BD-ITAC/processamento/confs/hadoop/* $HADOOP_HOME/etc/hadoop/
+rsync -av /usr/local/BD-ITAC/processamento/conf/hadoop/* $HADOOP_HOME/etc/hadoop/
 rsync -av /usr/local/BD-ITAC/processamento/setup/hadoop/sbin/* $HADOOP_HOME/sbin/
 chown -R hdfs:hdfs $HADOOP_HOME/
 
 # Hive
-rsync -av /usr/local/BD-ITAC/processamento/confs/hive/* $HIVE_HOME/conf/
+rsync -av /usr/local/BD-ITAC/processamento/conf/hive/* $HIVE_HOME/conf/
 rsync -av /usr/local/BD-ITAC/processamento/setup/hive/sbin/* $HIVE_HOME/sbin/
 chown -R hdfs:hdfs $HIVE_HOME/
 
 # Hue
-rsync -av /usr/local/BD-ITAC/processamento/confs/hue/* $HUE_HOME/desktop/conf/
+rsync -av /usr/local/BD-ITAC/processamento/conf/hue/* $HUE_HOME/desktop/conf/
 rsync -av /usr/local/BD-ITAC/processamento/setup/hue/sbin/* $HUE_HOME/build/env/sbin/
 chown -R hdfs:hdfs $HUE_HOME/
 
 # Setup dir
+rm /setup/*
 cp /usr/local/BD-ITAC/processamento/setup/* /setup/
 
 # Bashrc
-cp /usr/local/BD-ITAC/processamento/bashrc $HADOOP_HOME/.bashrc
-cp /usr/local/BD-ITAC/processamento/bashrc ~/.bashrc
+cp /usr/local/BD-ITAC/processamento/conf/bashrc $HADOOP_HOME/.bashrc
+cp /usr/local/BD-ITAC/processamento/conf/bashrc ~/.bashrc
 
 source ~/.bashrc
