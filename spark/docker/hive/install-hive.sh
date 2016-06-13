d111a6e76f4d4bd79b7a29186ee46cac54d32c74
scarenci <scarenci@gmail.com>
2016-06-13 16:00:04 -0300

    remocao da pasta do spark que passou a se chamar processamento

--- spark/docker/hive/install-hive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget --quiet http://mirrors.ukfast.co.uk/sites/ftp.apache.org/hive/hive-1.2.1/apache-hive-1.2.1-bin.tar.gz
-tar xzf apache-hive-1.2.1-bin.tar.gz -C /usr/local
-ln -s /usr/local/apache-hive-1.2.1-bin $HIVE_HOME
-rm apache-hive-1.2.1-bin.tar.gz
-
-chown -R $HADOOP_USER:$HADOOP_USER $HIVE_HOME/
