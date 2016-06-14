268884b78fd1f533dd332935c1efab11eafb2a17
algsilv <algsilv@gmail.com>
2016-06-14 11:24:14 -0300

    add pagina de sensores

--- api/public/js/controllers/sensorsCtrl.js
+++ api/public/js/controllers/sensorsCtrl.js
@@ -1,39 +1,19 @@
-/*
- * @author: edizon
- */
-
 'use strict';
-
-app.controller('CrisisController', ['$scope', '$rootScope', '$location', '$http','$localStorage',
-  'toastr','UtilService','Lightbox',
- function($scope,$rootScope,$location,$http,$localStorage, toastr, UtilService, Lightbox) {
+app.controller('SensorsController', ['$scope', '$rootScope', '$location', '$http',
+  '$localStorage',
+  'toastr','UtilService','Lightbox','ModalService',
+  function($scope,$rootScope,$location,$http,$localStorage, toastr, UtilService, Lightbox, ModalService) {
     var vm = this;
     vm.form = {submitted : false};
 
+    $scope.avisosSelecionados = [];
+    $scope.avisosListCollection = [];
+    
     UtilService.setCurrentMenu($scope, $location);
 
-    $http.get('/rest/crisis').success(function(data){
-                $scope.CrisisListCollection = data;
-        });
-
-
-
-        $scope.openLightboxModal = function (index, fotos) {
-        $scope.images = [];
-          angular.forEach(fotos, function(value)
-            {
-              if(value != null && value)
-              {
-              $scope.images.push({
-        'url': 'data:image/gif;base64,' + value,
-        'thumbUrl': 'data:image/gif;base64,' + value
-
+    $http.get('/rest/sensors').success(function(data){
+                $scope.avisosListCollection = data;
     });
-  }
-  });
+    
 
-          Lightbox.openModal($scope.images, index);
-        };
-
-
-}]);
+  }]);
