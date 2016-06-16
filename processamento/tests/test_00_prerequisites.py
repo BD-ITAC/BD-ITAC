cc5f6c84415372b7aad43aa9d23c342a99778838
Marcelo Gumercino Costa <marcelogc@gmail.com>
2016-06-15 21:18:18 -0300

    automatização do testinfra com parametrize

--- processamento/tests/test_00_prerequisites.py
+++ processamento/tests/test_00_prerequisites.py
@@ -1,3 +1,18 @@
+import pytest
+import os
+
+@pytest.mark.parametrize("variable", [
+        ("HADOOP_HOME"),
+        ("JAVA_HOME"),
+	("SPARK_HOME"),
+	("HIVE_HOME")])
+   
+def test_variable(File,variable):
+	dir = File(os.environ[variable])
+	assert os.environ[variable]
+	assert dir.is_directory
+
+
 def test_ssh_is_installed(Package):
     ssh = Package("openssh-server")
     assert ssh.is_installed
@@ -6,24 +21,3 @@ def test_ssh_running_and_enabled(Service):
     ssh = Service("ssh")
     assert ssh.is_running
     assert ssh.is_enabled
-
-def test_hadoophome_environment_variable(File):
-    environment = File("/etc/environment")
-    assert environment.is_file
-    assert environment.contains("HADOOP_HOME")
-
-def test_javahome_environment_variable(File):
-    environment = File("/etc/environment")
-    assert environment.is_file
-    assert environment.contains("JAVA_HOME")
-
-def test_hivehome_environment_variable(File):
-    environment = File("/etc/environment")
-    assert environment.is_file
-    assert environment.contains("HIVE_HOME")
-
-
-def test_hivehome_environment_variable(File):
-    environment = File("/etc/environment")
-    assert environment.is_file
-    assert environment.contains("SPARK_HOME")
